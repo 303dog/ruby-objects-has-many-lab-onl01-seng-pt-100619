7549416a0d9a488e1eb6355ef8719b8f0b9123d0
Paul Beckwith <303dog@gmail.com>
2019-11-17 22:50:03 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,28 +1,23 @@
 require 'pry'
 
 class Artist 
-  
-  attr_accessor :name, :songs
-  
-  def initialize(name)
-    @name =name
-    @songs = []
-  end
-  
-  def songs
-    @songs
-  end
-  
-  def add_song(song)
-    @songs << song
-    song.artist = self
-  end
-  
-   def add_song_by_name(song_name)
-     song.name = song_name
-   end
-   
-   def genres #returns an array of genre objects
+	attr_accessor :name, :songs
+	
+	def initialize(name)
+		@name = name
+		@songs = []
+	end
+	
+	def add_song(song)
+		@songs << song
+		song.artist = self
+	end
+	
+	def songs #used as self.songs in method below
+		@songs
+	end
+	
+	def genres #returns an array of genre objects
 		self.songs.collect do |song|
 			song.genre 
 		end
